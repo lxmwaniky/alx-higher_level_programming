8229929feb8bd7a5cf2c0d793a4b9919e2fec9d8
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-11-28 21:20:12 +0300

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i < 10:
-        print("0{}".format(i), end=", ")
-    elif i == 99:
-        print("{}".format(i))
-    else:
-        print("{}".format(i), end=", ")
+for i in range(100):
+    print("{:02d}".format(i), end=", " if i < 98 else "\n")
