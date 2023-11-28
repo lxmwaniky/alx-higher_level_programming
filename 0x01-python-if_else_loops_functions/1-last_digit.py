3ede1a974ddf4be19ecb3381ababb8ece757b7d6
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-11-28 16:27:32 +0300

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number > 0:
+if number < 0:
     last_digit = -int(str(number)[-1])
 else:
     last_digit = int(str(number)[-1])
