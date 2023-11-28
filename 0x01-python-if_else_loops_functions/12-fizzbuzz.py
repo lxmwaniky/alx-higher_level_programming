fc96f2be4ba297df8ae4ebb7b6433610fc7336ac
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-11-28 22:13:03 +0300

    12-fizzbuzz.py

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz', end=' ')
+            continue
+        elif i % 3 == 0:
+            print('Fizz', end=' ')
+            continue
+        elif i % 5 == 0:
+            print('Buzz', end=' ')
+            continue
+        else:
+            print('{}'.format(i), end=' ')
