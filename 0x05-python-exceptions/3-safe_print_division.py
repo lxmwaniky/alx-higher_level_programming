6107ade969f42c59e25adacf76e7d318b75e8234
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-12-18 20:59:28 +0300

    3-safe_print_division.py

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    
+    div = None
+    try:
+        div = a / b
+    except Exception:
+        return
+    finally:
+        if div is not None:
+            print('Inside result: {:.1f}'.format(div))
+        else:
+            print('Inside result: ', div)
+    return div
