e97efc5ef4c387e09a63f8639e1e0807986ef09b
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-12-18 18:53:12 +0300

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,11 @@ def safe_print_list(my_list=[], x=0):
         if x == 0:
                 print()
                 return 0
+
+    # track elements printed
         elements_printed = 0
-        
+
+    # iterate through list and print elements
         for element in my_list:
                 try:
                         print(f'{element}', end='')
