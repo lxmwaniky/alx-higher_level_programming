c5639021594288cffaa6cb23c02b3bac9dd4ed0f
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-12-18 20:57:35 +0300

    2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+
+    elements_printed = 0
+    for i in range(x):
+        try:
+            print('{:d}'.format(my_list[i]), end='')
+            elements_printed += 1
+        except (TypeError, ValueError):
+            pass
+    print()
+    return elements_printed
