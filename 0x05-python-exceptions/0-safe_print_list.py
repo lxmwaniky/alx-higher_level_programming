ddfe7de96fd7e18d3b5fd1d00e8e32169c87de91
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-12-18 18:50:47 +0300

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+        if x == 0:
+                print()
+                return 0
+        elements_printed = 0
+        
+        for element in my_list:
+                try:
+                        print(f'{element}', end='')
+                        elements_printed += 1
+                except exception as e:
+                        print(e)
+                if elements_printed == x:
+                        print()
+                        return elements_printed
+        print()
+        return elements_printed
