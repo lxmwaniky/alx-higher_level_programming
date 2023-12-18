212179c7eb6bf85cedcf7174799be5c25b1b91ef
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-12-18 21:14:57 +0300

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-        if x == 0:
-                print()
-                return 0
 
-        elements_printed = 0
-
-        for element in my_list:
-                try:
-                        print(f'{element}', end='')
-                        elements_printed += 1
-                except Exception as e:
-                        print(e)
-                if elements_printed == x:
-                        print()
-                        return elements_printed
+    if x == 0:
         print()
-        return elements_printed
+        return 0
+
+    elements_printed = 0
+
+    for element in my_list:
+        try:
+            print(f'{element}', end='')
+            elements_printed += 1
+        except exception as e:
+            print(e)
+        if elements_printed == x:
+            print()
+            return elements_printed
+    print()
+    return elements_printed
