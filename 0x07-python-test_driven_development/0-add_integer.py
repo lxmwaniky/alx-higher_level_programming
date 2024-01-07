a535cafb5b59702ba43c7a58c8e36a59dce24877
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-01-08 01:23:51 +0300

    Task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,12 +1,34 @@
 #!/usr/bin/python3
 
+"""
+This module gets the sum of two arguments that must be integers
+"""
+
+
 def add_integer(a, b=98):
+    """
+    Returns the sum of a and b
+
+    Arguments:
+    	a: first number
+	b: second number(Default value of 98)
+
+    Raises:
+    	TypeError: When Operands given are not valid -> Integers
+    
+    Returns:
+    	int: Sum of a and b
+    """
     if not isinstance(a, (int, float)):
         raise TypeError('a must be an integer')
-    elif not isinstance(b, (int, float)):
+    
+    if not isinstance(b, (int, float)):
         raise TypeError('b must be an integer')
-    elif isinstance(a, float):
-        a = int(a)
-    elif isinstance(b, float):
-        b = int(b)
-    return a + b
+    
+    try:
+       int(a)
+       int(b)
+    except Exception as e:
+       raise e
+
+    return int(a) + int(b)
