a35ac45c16e5c96e6cddddc5cddb9838d9b3fa03
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-03-12 00:00:19 +0300

    Refactor argument handling in 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-const args = process.argv;
-if (args.length < 3) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
-} else if (args.length < 4) {
-  console.log('Argument found');
 } else {
-  console.log('Arguments found');
+    console.log(process.argv[2]);
 }
