51e8647a7cc77b7dce464b829205c06fcd0b7702
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-03-11 23:55:28 +0300

    Refactor argument handling in 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 if (process.argv.length < 3) {
   console.log('No argument');
-}
-else if (process.argv.length < 4) {
+} else if (process.argv.length < 4) {
   console.log('Argument found');
-}
-else {
+} else {
   console.log('Arguments found');
 }
